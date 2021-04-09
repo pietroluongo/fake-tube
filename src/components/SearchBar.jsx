d0129ec578515057cef9a929dd7b2961505eadfe
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-09 18:16:29 -0300

    Implemented controlled component requirements on SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,16 +3,31 @@ import React from 'react';
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { term: '' };
+  }
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    // TODO: callback!
   }
 
   render() {
+    const { term } = this.state;
     return (
       <div className="search-bar ui segment">
-        <form action="" className="ui form">
+        <form action="" className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label htmlFor="videoSearch">Video Search</label>
-            <input type="text" id="videoSearch" />
+            <input
+              type="text"
+              value={term}
+              id="videoSearch"
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
