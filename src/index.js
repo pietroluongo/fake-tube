91a79c3a5da899a218bcbb17f9d227633004f7f5
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-04-09 18:27:30 -0300

    Added dotenv to store API keys; removed package-lock, as we're using yarn; updated .gitignore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
 ReactDOM.render(
   <React.StrictMode>
